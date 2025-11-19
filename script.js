2e4110a2c18caece417fd073b375bc17f82cdc6e
Jzaldvar <jzaldivar45@yahoo.com>
2025-11-19 15:59:17 -0800

    extra finished

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var pet_info = {
     name: "Cthulhu",
     weight: 6000,
-    happiness: 5
+    happiness: 5,
+    visibility: 1
+
 };
 
 
@@ -15,14 +17,23 @@ $(function() { // Makes sure that your function is called once all the DOM eleme
     $('.play-button').click(clickedPlayButton);
     $('.exercise-button').click(clickedExerciseButton);
     $('.hide-button').click(function () {
-        $('.pet-image').hide()
-    })
+        pet_info.happiness -= 1;       // hiding makes Cthulhu sad
+        pet_info.visibility = 0;
+
+        $('.pet-image').hide();
+        $('.message').text("Cthulhu got sad and hid.");
+        checkAndUpdatePetInfoInHtml();
+    });
 
     $('.show-button').click(function () {
-        $('.pet-image').show()
+        pet_info.happiness += 1;       // * showing makes Cthulhu happy
+        pet_info.visibility = 1;
 
+        $('.pet-image').show();
+        $('.message').text("Cthulhu happily shows its self.");
 
-    })
+        checkAndUpdatePetInfoInHtml();
+    });
 
     // Add a variable "pet_info" equal to an object with the name (string), weight (number), and happiness (number) of your pet
 
@@ -30,18 +41,21 @@ $(function() { // Makes sure that your function is called once all the DOM eleme
     function clickedTreatButton() {
         pet_info.happiness += 2;
         pet_info.weight += 1000;
+        $(".message").text("Cthulhu chomps the human and is happier.");
         checkAndUpdatePetInfoInHtml();
     }
 
     function clickedPlayButton() {
         pet_info.happiness += 3
         pet_info.weight -= 500
+        $(".message").text("Cthulhu plays with the puppies.");
         checkAndUpdatePetInfoInHtml();
     }
 
     function clickedExerciseButton() {
         pet_info.happiness -= 2
         pet_info.weight -= 750
+        $('.message').text("Cthulhu runs to the cult meeting.");
         checkAndUpdatePetInfoInHtml();
     }
 
@@ -64,5 +78,6 @@ $(function() { // Makes sure that your function is called once all the DOM eleme
         $('.name').text(pet_info['name']);
         $('.weight').text(pet_info['weight']);
         $('.happiness').text(pet_info['happiness']);
+        $('.visibility').text(pet_info.visibility === 1 ? "Visible" : "Hidden");
     }
 });
